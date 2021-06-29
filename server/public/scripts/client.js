4fc9ceed6b609410e7a868c882b3f6aa05756283
Duy Dao <jimmydao@gmail.com>
2021-06-29 18:49:47 -0500

    Error message gone after removing appendNewJokes from POST.then but array still not appending to DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,7 +3,6 @@ console.log('client.js sourced');
 $( document ).ready( onReady );
 
 function onReady() {
-    console.log('DOM ready');
     $('#addJokeButton').on('click', postJoke)
 }
 
@@ -32,8 +31,7 @@ function postJoke () {
         }) // end .ajax
         .then( function(response){
             console.log(response);
-            // refresh jokes
-            appendNewJokes(response)
+            // refresh jokes (removing the call for appendNewJokes from line 36)
         }) // end .then
         .catch(function (err){
             console.log('You got an error',err);
@@ -42,27 +40,25 @@ function postJoke () {
 
 function appendNewJokes (response){
 
-    //  $.ajax({
-    //         method: 'get',
-    //         url: '/newjoke',
-    //     }) // end .ajax
-    //     .then( function(response){
-    //         console.log('Here is the response from the server',response);
-    //         //loop the response array. This is where we can append.
-    //         for (let i = 0; i < response.jokes.length; i++) {
-    //             let newLi = $('<li>').text(response.jokes[i]);
-    //             $('#jokeCollection').append(newLi);
-    //         }
-    //     }) 
-    //     .catch(function (err){
-    //         console.log('You got an error',err);
-    //     });
-
-    $.get('/newjoke', function(response){
-    console.log(response);
-    for (let i = 0; i < response.length; i++) {
-      let newLi = $('<li>').text(response.jokes[i]);
-      $('#jokeCollection').append(newLi);
-    }
-  });
+     $.ajax({
+            method: 'get',
+            url: '/newjoke',
+        }) // end .ajax
+        .then( function(response){
+            //loop the response array. This is where we can append.
+            for (let i = 0; i < response.jokes.length; i++) {
+                let newLi = $('<li>').text(response.jokes[i]);
+                $('#jokeCollection').append(newLi);
+                console.log('What is the newLi doing?',response);
+                //Why wont this log show up in console? 
+            }
+        }) 
+        .catch(function (err){
+            console.log('You got an error',err);
+        });
+        console.log('Here is the response from the server',response);
+        //response coming back as Created. Sounds like it's getting a 201 
+        //status but that's in the app.post and not the app.get.
 }
+//removing appendNewJokes from line 36 resolved the error message but still 
+//not appending to DOM.
